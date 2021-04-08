ca33c166d5099f7b072c2e12cfbce2bcc5ad4e64
Sciencethebird <sciencethebird@gmail.com>
2021-04-08 13:28:19 +0800

    change README.md

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # git_practice
-I suck practice git lul
+
+
+
+# problems
+
+1. failed to push
+	'''
+	error: src refspec main does not match any.                      
+	error: failed to push some refs to 'git@github.com:Sciencethebird/DLP_Labs.git'
+	'''
+	solution:
+	
+	'''
+	git push <local_repo_name> HEAD:main
+	''' 
---
+++ git_examples.sh
@@ -0,0 +1,11 @@
+
+
+# problems
+
+1. failed to push
+	error: src refspec main does not match any.                                                                                                                                                                                               
+	error: failed to push some refs to 'git@github.com:Sciencethebird/DLP_Labs.git'
+
+	solution:
+	
+
